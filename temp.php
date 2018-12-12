35b0a658b701538fb5f0dd868e59c889d1518923
Doond <mdoond69h@gmail.com>
2018-12-12 20:56:31 +0900

    テンプレファイル追加しました

---
+++ temp.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<HTML lang="ja">
+  <HEAD>
+    <meta charset="utf-8">
+    <title>GDSS ゴミ出し支援システム</title>
+    <link rel="icon" href="iconG.ico">
+    <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+    <link rel="stylesheet" href="design.css">
+  </HEAD>
+  <BODY>
+    <!-- HEADER -->
+    <div class="header">
+      <p class="title">GDSS</p>
+      <p class="wayaku">ゴミ出し支援システム</p>
+      <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
+    </div>
+
+    <!-- メインコンテンツエリア -->
+    <div class="content">
+      <!-- ここに各ページの中身いれてください -->
+    </div>
+
+    <!-- PAGE TOPに戻るボタン
+  ぺーじによっては、コメントアウトして消してください -->
+    <a class="pagetop" href="#">PAGE TOP</a>
+
+    <!-- FOOTER -->
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div>
+  </BODY>
+</HTML>

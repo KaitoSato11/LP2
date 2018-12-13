9607ff49dfdff230c9d3b1a3f2ccb28aeef2d3a0
1200293y <200293y@ugs.kochi-tech.ac.jp>
2018-12-13 14:50:02 +0900

    Main画面

---
+++ Main.php
@@ -0,0 +1,21 @@
+<?php
+echo "Hello Everyone.";
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<title>Event Management System</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<div align="center">
+			<h1>Welcome to GDSS!</h1>
+			<a href="./Login.php">Login</a>
+		</div>
+		<br><br><br>
+		<div align="right">
+			LastUpdate is 2018/12/7 16:30
+		</div>
+	</body>
+</html>

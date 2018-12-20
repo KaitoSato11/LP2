960b20fdc36c5aa3230eecf911529efb9c9e5411
1200365 <kitako.ex24@icloud.com>
2018-12-20 10:18:05 +0900

    ちょっと変更

--- Admin_edit_cal.php
+++ Admin_edit_cal.php
@@ -22,30 +22,34 @@ $options = array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET CHARACTER SET 'utf8'");
 
 error_reporting(E_ALL & ~E_NOTICE);
 
-$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+$dsn = 'mysql:dbname='.$dbname.';host='.$host.';charset=utf8';
 
 #データベースに接続
 
-try {
-$pdo = new PDO($dns, $user, $pass,
-array(PDO::ATTR_EMULATE_PREPARES => false));
-} catch (PDOException $e) {
-exit('データベースとの接続に失敗しました。'.$e->getMessage());
+
+
+//画面遷移
+if(isset($_POST["henkou"])) {
+	$check = 1;
 }
 
 ?>
 
 <!DOCTYPE html>
 <HTML lang="ja">
-  <HEAD>
-    <meta charset="utf-8">
-    <title>GDSS ゴミ出し支援システム</title>
-    <link rel="icon" href="iconG.ico">
-    <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
-    <link rel="stylesheet" href="design.css">
-    <style type="text/css">
-    </style>
-  </HEAD>
+	<!--入力画面-->
+	<?php
+	if($check == 0) {
+	?>
+	<HEAD>
+		<meta charset="utf-8">
+		<title>GDSS ゴミ出し支援システム</title>
+		<link rel="icon" href="iconG.ico">
+		<meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+		<link rel="stylesheet" href="design.css">
+		<style type="text/css">
+		</style>
+	</HEAD>
 	<BODY>
     <!-- HEADER -->
     <div class="header">
@@ -61,65 +65,133 @@ exit('データベースとの接続に失敗しました。'.$e->getMessage());
       <h3><a href="Mypage.php">
         <input type="submit" value="マイページに戻る"/>
       </a></h3>
-
       <table border="1">
       <tr><td colspan="100%">カレンダー編集</td></tr>
       <tr>
         <td width="200px">地区名</td>
-        <td width="700px">〇〇地区</td>
+        <td width="500px">〇〇地区</td>
       </tr>
 			<tr>
         <td>種類</td>
         <td>〇〇ゴミ</td>
       </tr>
 			<tr>
-        <td height="150px">曜日</td>
-        <td height="150px">
+        <td height="250px">曜日</td>
+        <td height="250px">
+					<form method="POST" action="">
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第1日曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第2日曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第3日曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第4日曜日
+					<br>
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第1月曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第2月曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第3月曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第4月曜日
+					<br>
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第1火曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第2火曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第3火曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第4火曜日
+					<br>
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第1水曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第2水曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第3水曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第4水曜日
+					<br>
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第1木曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第2木曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第3木曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第4木曜日
+					<br>
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第1金曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第2金曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第3金曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第4金曜日
+					<br>
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第1土曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第2土曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第3土曜日
+					<input type="hidden" name="week1[]" value="0">
+					<input type="checkbox" onclick="this.form.week1[].value=this.checked ? 1 : 0">第4土曜日
+					<!--
 					<input type="checkbox" name="week[]" value="sun1">第1日曜日
-					<input type="checkbox" name="week[]" value="mon1">第1月曜日
-					<input type="checkbox" name="week[]" value="tue1">第1火曜日
-					<input type="checkbox" name="week[]" value="wed1">第1水曜日
-					<input type="checkbox" name="week[]" value="thu1">第1木曜日
-					<input type="checkbox" name="week[]" value="fri1">第1金曜日
-					<input type="checkbox" name="week[]" value="sat1">第1土曜日
-					<br>
 					<input type="checkbox" name="week[]" value="sun2">第2日曜日
-					<input type="checkbox" name="week[]" value="mon2">第2月曜日
-					<input type="checkbox" name="week[]" value="tue2">第2火曜日
-					<input type="checkbox" name="week[]" value="wed2">第2水曜日
-					<input type="checkbox" name="week[]" value="thu2">第2木曜日
-					<input type="checkbox" name="week[]" value="fri2">第2金曜日
-					<input type="checkbox" name="week[]" value="sat2">第2土曜日
-					<br>
 					<input type="checkbox" name="week[]" value="sun3">第3日曜日
-					<input type="checkbox" name="week[]" value="mon3">第3月曜日
-					<input type="checkbox" name="week[]" value="tue3">第3火曜日
-					<input type="checkbox" name="week[]" value="wed3">第3水曜日
-					<input type="checkbox" name="week[]" value="thu3">第3木曜日
-					<input type="checkbox" name="week[]" value="fri3">第3金曜日
-					<input type="checkbox" name="week[]" value="sat3">第3土曜日
-					<br>
 					<input type="checkbox" name="week[]" value="sun4">第4日曜日
+					<br>
+					<input type="checkbox" name="week[]" value="mon1">第1月曜日
+					<input type="checkbox" name="week[]" value="mon2">第2月曜日
+					<input type="checkbox" name="week[]" value="mon3">第3月曜日
 					<input type="checkbox" name="week[]" value="mon4">第4月曜日
+					<br>
+					<input type="checkbox" name="week[]" value="tue1">第1火曜日
+					<input type="checkbox" name="week[]" value="tue2">第2火曜日
+					<input type="checkbox" name="week[]" value="tue3">第3火曜日
 					<input type="checkbox" name="week[]" value="tue4">第4火曜日
+					<br>
+					<input type="checkbox" name="week[]" value="wed1">第1水曜日
+					<input type="checkbox" name="week[]" value="wed2">第2水曜日
+					<input type="checkbox" name="week[]" value="wed3">第3水曜日
 					<input type="checkbox" name="week[]" value="wed4">第4水曜日
+					<br>
+					<input type="checkbox" name="week[]" value="thu1">第1木曜日
+					<input type="checkbox" name="week[]" value="thu2">第2木曜日
+					<input type="checkbox" name="week[]" value="thu3">第3木曜日
 					<input type="checkbox" name="week[]" value="thu4">第4木曜日
+					<br>
+					<input type="checkbox" name="week[]" value="fri1">第1金曜日
+					<input type="checkbox" name="week[]" value="fri2">第2金曜日
+					<input type="checkbox" name="week[]" value="fri3">第3金曜日
 					<input type="checkbox" name="week[]" value="fri4">第4金曜日
+					<br>
+					<input type="checkbox" name="week[]" value="sat1">第1土曜日
+					<input type="checkbox" name="week[]" value="sat2">第2土曜日
+					<input type="checkbox" name="week[]" value="sat3">第3土曜日
 					<input type="checkbox" name="week[]" value="sat4">第4土曜日
+				  -->
 				</td>
       </tr>
       </table>
     </div>
 		<h4>
 			<a href="Admin_edit_cal_comp.php">
-				<input type="submit" value="変更"/>
+				<input type="submit" id="henkou" name="henkou" value="変更"/>
 			</a>
 		</h4>
 		<h5>
 			<a href="Admin_edit_cal_comp.php">
-				<input type="submit" value="削除"/>
+				<input type="submit" id="sakujo" name="sakujo" value="削除"/>
 			</a>
 		</h5>
+	</form>
 
     <!-- PAGE TOPに戻るボタン
   ぺーじによっては、コメントアウトして消してください -->
@@ -131,4 +203,94 @@ exit('データベースとの接続に失敗しました。'.$e->getMessage());
       <p class="company">L&P</p>
     </div>
   </BODY>
+	<?php
+	}
+	?>
+
+	<!--変更確認画面-->
+	<?php
+	if($check == 1) {
+	?>
+	<HEAD>
+		<meta charset="utf-8">
+		<title>GDSS ゴミ出し支援システム</title>
+		<link rel="icon" href="iconG.ico">
+		<meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+		<link rel="stylesheet" href="design.css">
+		<style type="text/css">
+		</style>
+	</HEAD>
+<BODY>
+
+	<!-- PAGE TOPに戻るボタン
+ぺーじによっては、コメントアウトして消してください -->
+	<a class="pagetop" href="#">PAGE TOP</a>
+
+	<!-- FOOTER -->
+	<div class="footer">
+		<p class="title">GDSS</p>
+		<p class="company">L&P</p>
+	</div>
+</BODY>
+<?php
+}
+?>
+
+<!--削除確認画面-->
+<?php
+if($check == 2) {
+?>
+<HEAD>
+	<meta charset="utf-8">
+	<title>GDSS ゴミ出し支援システム</title>
+	<link rel="icon" href="iconG.ico">
+	<meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+	<link rel="stylesheet" href="design.css">
+	<style type="text/css">
+	</style>
+</HEAD>
+<BODY>
+
+<!-- PAGE TOPに戻るボタン
+ぺーじによっては、コメントアウトして消してください -->
+<a class="pagetop" href="#">PAGE TOP</a>
+
+<!-- FOOTER -->
+<div class="footer">
+	<p class="title">GDSS</p>
+	<p class="company">L&P</p>
+</div>
+</BODY>
+<?php
+}
+?>
+
+<!--エラー画面-->
+<?php
+if($check == 3) {
+?>
+<HEAD>
+	<meta charset="utf-8">
+	<title>GDSS ゴミ出し支援システム</title>
+	<link rel="icon" href="iconG.ico">
+	<meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+	<link rel="stylesheet" href="design.css">
+	<style type="text/css">
+	</style>
+</HEAD>
+<BODY>
+
+<!-- PAGE TOPに戻るボタン
+ぺーじによっては、コメントアウトして消してください -->
+<a class="pagetop" href="#">PAGE TOP</a>
+
+<!-- FOOTER -->
+<div class="footer">
+	<p class="title">GDSS</p>
+	<p class="company">L&P</p>
+</div>
+</BODY>
+<?php
+}
+?>
 </HTML>

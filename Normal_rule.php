912dc01bf66ef7a85a969f8dc62be6bda6b064b9
satozaki <nuko0310@gmail.com>
2018-12-26 17:05:45 +0900

    ver1 ゴミルール表  とりあえず型と文字だけ

---
+++ Normal_rule.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<HTML lang="ja">
+
+<HEAD>
+  <meta charset="utf-8">
+  <title>GDSS ゴミ出し支援システム</title>
+  <link rel="icon" href="iconG.ico">
+  <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+  <link rel="stylesheet" href="./css/design_satozaki.css">
+  <!-- <link rel="stylesheet" href="design.css"> -->
+</HEAD>
+
+<BODY>
+  <!-- HEADER -->
+  <div class="header">
+    <p class="title">GDSS</p>
+    <p class="wayaku">ゴミ出し支援システム</p>
+    <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
+  </div>
+
+  <!-- メインコンテンツエリア -->
+  <div class="content">
+    <!-- ここに各ページの中身いれてください -->
+    <div class="bb1">
+      <div class="b5">
+        <h3><a href="Mypage.php">マイページに戻る</h3></a>
+      </div>
+    </div>
+
+    <div class="Spesial">
+      <div class="line">
+        <h2></h2>
+      </div>
+      <div class="title1">
+        <h2>粗大・特別ゴミルール</h2>
+      </div>
+      <table class="t02" border="1" width="100%">
+        <tr>
+          <th></th>
+          <th width="3%" align="center" colspan="2">分別</th>
+          <th width="5%">収集日</th>
+          <th colspan="2">出せるもの</th>
+          <th width="20%">注意点</th>
+          <th width="5%">使用できるゴミ袋</th>
+        </tr>
+        <tr>
+          <td rowspan="9" width="2%">ゴミステーションに出せるもの</td>
+          <td colspan="2">燃えるゴミ</td>
+          <td align="center">
+            毎週
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>・
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>
+            曜日
+          </td>
+          <td colspan="2"></td>
+          <td>
+            <ul>
+              <li><u>ビン・缶・硬質プラスチックなどの燃えないゴミは絶対に入れないでください。</u></li>
+              <li>生ゴミは十分に水切りしてください</li>
+              <li>草木などは太さ3cm、長さ70cm以内に切断してください</li>
+              <li>紙おむつの汚物はは取り除いてください</li>
+              <li>布団・毛布・布じゅうたんなどは6等分に切ってください</li>
+            </ul>
+          </td>
+          <td>白色の燃えるゴミ指定袋<br>
+            大・中・小</td>
+        </tr>
+        <tr>
+          <td rowspan="7">資源ゴミ</td>
+          <td>金属類</td>
+          <td>
+            毎週
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>・
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>
+            曜日
+          </td>
+
+          <td align="center" valign="top">
+            金属類(飲料用の缶)　アルミ缶・スチール缶<br>
+            <img src="./img/keikoutou.png" alt="蛍光灯" width="100">
+            <p>
+              <ul>
+                <li>中身を出して水洗いしてください</li>
+                <li>飲料用の缶は、アルミ缶とスチール缶に分ける必要はありません</li>
+              </ul>
+            </p>
+          </td>
+          <td align="center">
+            金属類(その他)<br>
+            <img src="./img/keikoutou.png" alt="蛍光灯" width="100"><br>
+            <ul>
+              <li>飲料用の缶以外は飲料用の缶と袋を分ける必要があります</li>
+              <li>刃物は危険のないように紙等で包み、袋に表示して出してください</li>
+              <li>スプレー缶・カセットボンベは、必ず使い切って穴を開けてください</li>
+            </ul>
+          </td>
+          <td>
+            <ul>
+              <li>中身を出して水洗いしてください</li>
+              <li>飲料用の缶は、アルミ缶とスチール缶に分ける必要はありません</li>
+            </ul>
+          </td>
+          <td>透明の資源ゴミ指定袋<br>
+            中・小</td>
+        </tr>
+        <tr>
+          <td>
+            ビン類
+          </td>
+          <td>
+            毎週
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>・
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>
+            曜日
+          </td>
+          <td>
+            透明ビン・茶色ビン・その他の色のビン(食品用・飲み薬用)<br>
+            <img src="./img/keikoutou.png" alt="蛍光灯" width="100"><br>
+            <ul>
+              <li>ビンの色ごとに分ける必要はありません</li>
+              <li>中身を出して水洗いしてください</li>
+              <li>キャップは必ず取って、金属製のキャップは金属(その他)へ、プラスチック製のキャップは容器包装プラスチックへ入れてください</li>
+            </ul>
+          </td>
+          <td>
+            <img src="./img/keikoutou.png" alt="蛍光灯" width="100"><br>
+            化粧品用・劇薬剤用のビンや割れたビン、コップや灰皿などのガラス製品はその他の不燃物へ入れてください</td>
+          <td>
+            <ul>
+              <li>ビンの色ごとに分ける必要はありません</li>
+              <li>中身を出して水洗いしてください</li>
+              <li>キャップは必ず取って、金属製キャップは金属(その他)へ、プラスチック製のキャップは容器包装プラスチックへ入れてください</li>
+              <li>化粧品用・劇薬剤用のビンや割れたビン、コップや灰皿などのガラス製品はその他の不燃物へいれてください</li>
+            </ul>
+          </td>
+          <td>透明の資源ゴミ指定袋<br>
+            中・小
+          </td>
+        </tr>
+
+        <tr>
+          <td>その他の不燃物</td>
+          <td>
+            毎週
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>・
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>
+            曜日
+          </td>
+          <td colspan="2"></td>
+          <td>
+            <ul>
+              <li>割れた物は新聞紙等に包んで危険のないよう袋に表示してください</li>
+              <li>おもちゃの電池は除いてください(水銀を含むゴミへ)</li>
+              <li>ライターのガスはすべて抜いてください</li>
+            </ul>
+          </td>
+          <td>透明の資源ゴミ指定袋<br>
+            中・小</td>
+        </tr>
+
+        <tr>
+          <td>ペットボトル</td>
+          <td> 毎週
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>・
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>
+            曜日
+          </td>
+          <td colspan="2"></td>
+          <td>
+            <ul>
+              <li>キャップとラベルは必ず取って、中身を出して水洗いしてください</li>
+              <li>プラスチック製のキャップ及びラベルは容器包装プラスチックへ入れてください</li>
+            </ul>
+          </td>
+          <td>透明の資源ゴミ指定袋<br>
+            大・中・小</td>
+        </tr>
+
+        <tr>
+          <td>容器包装プラスチック</td>
+          <td>
+            毎週
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>・
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>
+            曜日
+          </td>
+          <td colspan="2"></td>
+          <td>
+            <ul>
+              <li><u>商品を包むプラスチック製でできた容器・包装</u>がリサイクルの対象になります</li>
+              <li>中身を出して水洗いしてください</li>
+              <li>レジ袋に入れて出さないでください</li>
+            </ul>
+          </td>
+          <td>透明の資源ゴミ指定袋<br>
+            大・中・小</td>
+        </tr>
+
+        <tr>
+          <td>紙類</td>
+          <td>
+            毎週
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>・
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>
+            曜日
+          </td>
+          <td colspan="2"></td>
+          <td>
+            <ul>
+              <li>紙類は、ひもで縛って出してください(ひも以外のものを使わないでください)</li>
+              <li>酒パック等は可燃ごみへ入れてください</li>
+            </ul>
+          </td>
+          <td>ひもで縛って出してください</td>
+        </tr>
+
+        <tr>
+          <td>衣類</td>
+          <td>
+            毎週
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>・
+            <div style="padding: 30px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px;"></div>
+            曜日
+          <td colspan="2"></td>
+          <td>
+            <ul>
+              <li>衣類は、雨の日に出さないでください</li>
+              <li>衣類は透明の資源ゴミ指定袋に入れてください</li>
+              <li><u>下着、タオル、カーテン、じゅうたん、布団、毛布、座布団等や汚れたものは除きます</u>
+                (燃えるゴミか粗大ごみへ)</li>
+            </ul>
+          </td>
+          <td>透明の資源ゴミ指定袋<br>
+            大・中・小</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+
+    <!-- PAGE TOPに戻るボタン
+  ぺーじによっては、コメントアウトして消してください -->
+    <a class="pagetop" href="#">PAGE TOP</a>
+
+    <!-- FOOTER -->
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div>
+</BODY>
+
+</HTML>

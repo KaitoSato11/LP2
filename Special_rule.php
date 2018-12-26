1ba5ba343d20c9a49958f3531ea042dcb3bc1786
satozaki <nuko0310@gmail.com>
2018-12-26 17:04:22 +0900

    ver1 粗大・特別ゴミルール とりあえず型と文字だけ

---
+++ Special_rule.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<HTML lang="ja">
+
+<HEAD>
+  <meta charset="utf-8">
+  <title>GDSS ゴミ出し支援システム</title>
+  <link rel="icon" href="iconG.ico">
+  <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+  <link rel="stylesheet" href="./css/design_satozaki.css">
+  <!-- <link rel="stylesheet" href="design.css"> -->
+</HEAD>
+
+<BODY>
+  <!-- HEADER -->
+  <div class="header">
+    <p class="title">GDSS</p>
+    <p class="wayaku">ゴミ出し支援システム</p>
+    <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
+  </div>
+
+  <!-- メインコンテンツエリア -->
+  <div class="content">
+    <!-- ここに各ページの中身いれてください -->
+    <div class="bb1">
+      <div class="b5">
+        <h3><a href="Mypage.php">マイページに戻る</h3></a>
+      </div>
+    </div>
+
+    <div class="Spesial">
+      <div class="line">
+        <h2></h2>
+      </div>
+      <div class="title1">
+        <h2>粗大・特別ゴミルール</h2>
+      </div>
+      <div class="rule1">
+        <table class="t01" border="1" width="100%">
+          <tr>
+            <th></th>
+            <th width="4%" align="center">分別</th>
+            <th width="6%">収集日</th>
+            <th colspan="5">出せるもの</th>
+            <th width="">注意点</th>
+          </tr>
+          <tr>
+            <td rowspan="8" width="2%">直接持ち込むもの</td>
+            <td rowspan="1">水銀を含むゴミ</td>
+            <td rowspan="1">随時持ち込み</td>
+            <td class="a1" colspan="4" style="border-right: hidden" width="50">
+              <img src="./img/keikoutou.png" alt="蛍光灯" width="100">
+              　　<img src="./img/denchi.png" alt="蛍光灯" width="100" height="100">
+              　　<img src="./img/taionkei.png" alt="蛍光灯" width="100" height="100">
+              　<img src="./img/kagami.png" alt="蛍光灯" width="100" height="100">
+              <p>蛍光灯　　　　　　乾電池　　　　　体温計　　　　　鏡　</p>
+            <td width="200" rowspan="1">
+              <p>出せる場所</p>
+              <ul>
+                <li>香美市本庁舎北側通用口前</li>
+                <li>プラザハ八王子1階</li>
+                <li>ふれあい交流センター</li>
+                <li>繁藤出張所</li>
+                <li>香北支所</li>
+                <li>物部支所</li>
+              </ul>
+            </td>
+            <td rowspan="1">
+              <ul>
+                <li>充電式電池は捨てないでください。(ホームセンターマルン山田店・マツヤデンキ土佐山田店などのリサイクル協力店へ)</li>
+                <li>ライターは入れないでください。(その他の不燃物へ)</li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <td rowspan="7">粗大ごみ</td>
+            <td rowspan="7">毎月第3回目の日曜日と翌日の月曜日(香北町永野粗大ごみ置き場については第3回目の日曜日のみ)
+            9:30~12:00
+            13:00~16:00
+            </td>
+
+            <td width="15%" align="center"> 持ち込めるもの</td>
+            <td width="15%" align="center">持ち込めないもの</td>
+            <td colspan="3" align="center">持ち込み手数料(1台につき)</td>
+            <td rowspan="7">
+              <ul>
+                <li>持ち込み場所・日時</li>
+                市立一般廃棄物処理場<br>
+                (鏡野中学校東側の道を北へと約600m)<br>
+                毎月第3回目の日曜日と翌日の月曜日<br>
+                9:30~12:00 ・ 13:00~16:00<br><br>
+                香北町永野粗大ごみ仮置き場<br>
+                (高照寺から東へ約100m)<br>
+                毎月第3回目の日曜日<br>
+                9:30~12:00 ・ 13:00~16:00<br><br>
+                <li>ゴミ収集ステーションに出せるもの(燃えるゴミ、金属・ビン、ペットボトル、容器包装プラスチック、紙類、衣類、その他の不燃物)は持ち込みできません。きちんと分別して指定日にステーションへ出してください。</li>
+                <li>家電4品目(テレビ、エアコン、冷蔵庫・冷凍庫、洗濯機・衣類乾燥機)は持ち込みできません。リサイクル料金等を支払い小売店等で引き取ってもらってください。</li>
+                <li>パソコン(ノート含)本体とモニタは持ち込みできません。メーカに連絡して送ることになります。リサイクルシールのないものはリサイクル料金がかかります。</li>
+                <li>料金は、量の多少にかかわらず1車単位です。ご近所、町内会で声を掛け合って持ち込んでください。</li>
+                <li>事業所(商店、事務所、自営業)など営業活動により出るごみについては持ち込めません。事業所ごみは、事業者自らの責任において適正に処理しなければなりません。</li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <td rowspan="6">
+              <ul>
+                <li>金属類(カンは除く)</li>
+                <li>ビデオ・掃除機などの電化製品(ただし家電四品は除く)</li>
+                <li>プラスチック製不燃物(指定袋に入らないもの)</li>
+                <li>タンス・机等木製品</li>
+                <li>自転車</li>
+                <li>発砲スチロール</li>
+                <li>鏡・ガラス・陶磁器等</li>
+                <li>蛍光灯・乾電池</li>
+              </ul>
+            </td>
+            <td rowspan="6">
+              <ul>
+                <li>カン・ビンペットボトル等ステーション収集するもの</li>
+                <li>商店・事務所・工場等から出るゴミ</li>
+                <li>農家から出る農機具・ハウス園芸用資材・農薬缶等</li>
+                <li>タイヤ・バッテリー等(車やバイク関係のもの)</li>
+                <li>LPガスボンベ・消火器</li>
+                <li>中身のある油缶・塗料缶</li>
+                <li>家の改築・模様替え等によるごみ(瓦・土・ブロック等)</li>
+                <li>建築廃材及び灰等</li>
+              </ul>
+            </td>
+            <td>一輪車、台車、自転車又は自動二輪者による搬入</td>
+            <td>200円</td>
+            <td>自転車の画像</td>
+          </tr>
+
+          <tr>
+            <td>乗用車及び最大積載量250kg以下の車両による搬入</td>
+            <td>1000円</td>
+            <td>乗用車の画像</td>
+          </tr>
+
+          <tr>
+            <td>最大積載量251kg異常500kg以下の車両による搬入</td>
+            <td>2000円</td>
+            <td>軽トラの画像</td>
+          </tr>
+
+          <tr>
+            <td>最大積載量</td>
+            <td>4100円</td>
+            <td>画像</td>
+          </tr>
+
+          <tr>
+            <td>最大積載量</td>
+            <td>8200円</td>
+            <td>画像</td>
+          </tr>
+
+          <tr>
+            <td colspan="3">※以後、最大積載量が1,000kg増えるごとに4100円加算</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+
+
+
+    <!-- PAGE TOPに戻るボタン
+  ぺーじによっては、コメントアウトして消してください -->
+    <a class="pagetop" href="#">PAGE TOP</a>
+
+    <!-- FOOTER -->
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div> -->
+</BODY>
+
+</HTML>

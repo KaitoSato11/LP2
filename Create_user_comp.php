33df748834fe8d51d7087176f63d412600cf98b7
1200293y <200293y@ugs.kochi-tech.ac.jp>
2018-12-29 13:03:30 +0900

    ユーザ登録完了画面大体完成

---
+++ Create_user_comp.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$errorMessage ="";
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>新規登録完了</title>
+</head>
+<body>
+<center>
+<h2>新規登録が完了しました</h2>
+</center>
+<div id="event">
+<a href="./Main.php">メインメニューへ戻る</a>
+</div>
+</body>
+</html>

464ec2e59b9be26ed415bf66f5a2687e41841e7b
1200365 <kitako.ex24@icloud.com>
2019-01-05 19:16:26 +0900

    簡単に書いただけやけど、いいかな？

---
+++ Admin_edit_cal_comp.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<HTML lang="ja">
+	<!--入力画面-->
+	<HEAD>
+		<meta charset="utf-8">
+		<title>GDSS ゴミ出し支援システム</title>
+		<link rel="icon" href="iconG.ico">
+		<meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+		<link rel="stylesheet" href="design.css">
+		<style type="text/css">
+		</style>
+	</HEAD>
+	<BODY>
+    <!-- HEADER -->
+    <div class="header">
+      <p class="title">GDSS</p>
+      <p class="wayaku">ゴミ出し支援システム</p>
+      <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
+    </div>
+
+		<!-- メインコンテンツエリア -->
+    <div class="content">
+      <!-- ここに各ページの中身いれてください -->
+      <div>
+				<h1>完了しました！</h1>
+      <h3><a href="Mypage.php">
+        <input type="submit" value="マイページに戻る"/>
+      </a></h3>
+			<h4><a href="Admin_list_cal.php">
+        <input type="submit" value="カレンダー一覧に戻る"/>
+      </a></h4>
+		</div>
+	</div>
+	<!-- FOOTER -->
+	<div class="footer">
+		<p class="title">GDSS</p>
+		<p class="company">L&P</p>
+	</div>
+</BODY>
+</HTML>

930558b68595b25f1b714d7b19f85874d511d12c
htkz1035 <32477067+htkz1035@users.noreply.github.com>
2019-01-05 21:20:03 +0900

    メールリマインド

--- Mail.php
+++ Mail.php
@@ -15,51 +15,56 @@
 </div>
 
 <?php
-require_once('config.php');
 session_start();
 $check=0;
 $errorMessage="";
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $mail_adress = $_POST['mail_adress'];
-    $remind01 = $_POST['remind01'];
-}
 
-(isset($_POST['signup'])){
+
+if(isset($_POST['signup'])){
 $db_name = "mysql:db_name=gdss_db;host=localhost";
 $db_username = "root";
 $db_password = "";
 $db = new PDO($db_name, $db_username, $db_password);
 $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stmt = $db -> perepare("INSERT INTO users(address, remind) VALUES (:mail_adress, :remind)");
-$stmt -> bindParam(":mail_adress", $mail_adress, PDO::PARAM_STR);
+$stmt = $db -> perepare("INSERT INTO users(address, remind) VALUES (:mail_address, :remind)");
+$stmt -> bindParam(":mail_address", $mail_address, PDO::PARAM_STR);
 $stmt -> bindValue(":remind", $remind01, PDO::PARAM_INT);
 $stmt -> execute();
 header("Location:Mail_comp.php");
 exit();
 }
 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $mail_address = $_POST['mail_address'];
+    $remind01 = $_POST['remind01'];
+}
+
 //----------画面遷移----------
 if(isset($_POST['result'])){
-	if(empty($_POST['mail_adress']) && ($_POST['remind01']) && ($POST['remind01']== "1")){
+  if($_POST['remind01']=="null"){
+    $check=0;
+  	$errorMessage .= "受け取り可否を決めてください";
+	}else if((empty($_POST['mail_address'])) && ($_POST['remind01']) && ($_POST['remind01']== "1")){
 	$check=0;
-	$errorMessage .= "メールアドレスを入力してください"
+	$errorMessage .= "メールアドレスを入力してください";
 }else{
   $check=1;
-	}
+  $errorMessage="";
+}
+}
 	if(isset($_POST['back'])){
     	$check = 0;
 	}
 ?>
 
-<?php if($check == 0){ ?>
+<?php if($check == "0"){ ?>
 <body>
+  <h3><?php echo $errorMessage; ?></h3><br>
   <div class="content">
-      <h2><?php echo $errorMessage; ?></h2><br>
-	<form method="POST" action="">
-    <table width="800" height="400" border="1" cellspacing="0" align="center">
-      <tr height="50" bgcolor="#8fc27a">
+    <table width="1024" height="400"  cellspacing="0" align="center">
+      <tr width="1024" height="50" bgcolor="#8fc27a">
 	<th>メールリマインド設定</th>
       </tr>
       <td align="center" valign="middle" bgcolor="#fff8e0">
@@ -71,13 +76,14 @@ if(isset($_POST['result'])){
 	    </td>
 	  </tr>
 	  <td align="center" valign="middle">
-      <input type="email" size="30" id="mail_adress" name="mail_adress" id="mail_adress" maxlength="50" placeholder="メールアドレス入力">
+      <form method="POST" action="" >
+      <input type="email" size="30" id="mail_adress" name="mail_address" id="mail_address" maxlength="50" placeholder="メールアドレス入力">
       	      </td>
       	    </table>
       	    <input type="radio" id="remind01" name="remind01"  value="1">受け取る
       	      <input type="radio" id="remind01" name="remind01"  value="0">受け取らない
       	      </p>
-      	      <p><input type="submit" name="result" style="background-color:#8fc27a" value="変更・登録"></p>
+      	      <p><input type="submit" id="result" name="result" style="background-color:#8fc27a" value="変更・登録"></p>
       	      </form>
       	    </td>
       	  </table>
@@ -86,24 +92,24 @@ if(isset($_POST['result'])){
 <?php } ?>
 
 
-<?php if($check == 1){ ?>
+<?php if($check == "1"){ ?>
   <body>
     <div align="center">
-      <div class="box gray">
-        <form id="check_adress" name="check_adress" action="" method="POST">
+      <div class="content">
+        <form id="check_address" name="check_address" action="" method="POST">
   	<table>
-  	  <tr><td>メールアドレス</td><td><?php echo $_POST['mail_adress']; ?></td></tr>
-  	  <tr><td>リマインド受け取り可否</td><td><?($POST[remind01]== "1"){受け取る}else{受け取らない} ?></td></tr>
+  	  <tr><td>メールアドレス</td><td><?php echo $_POST['mail_address']; ?></td></tr>
+  	  <tr><td>リマインド受け取り可否</td><td><?php if($_POST['remind01']== "1"){ ?>受け取る<?php }else{?> 受け取らない<?php } ?></td></tr>
             </table>
 
-  	<input type="hidden" id="mail_adress" name="mail_adress" size="30" value="<?php echo $_POST['mail_adress'];?>">
-  	  <input type="hidden" id="remind01" name="remind01" value="<?php echo $_POST['remind01'];}  ?>">
+  	<input type="hidden" id="mail_address" name="mail_address" size="30" value="<?php echo $_POST['mail_address'];?>">
+  	  <input type="hidden" id="remind01" name="remind01" value="<?php echo $_POST['remind01']; ?>">
               <input type="submit" value="登録" name="signup"  style="WIDTH:70px; HEIGHT:30px;">
   	    </form>
 
   	    <form action="" method="POST">
-  	      <input type="hidden" id="mail_adress" name="mail_adress" size="30" value="<?php echo $_POST['mail_adress']; ?>">
-  		<input type="hidden" id="remind01" name="remind01" value="<?php echo $_POST['remind01'];}  ?>">
+  	      <input type="hidden" id="mail_address" name="mail_address" size="30" value="<?php echo $_POST['mail_address']; ?>">
+  		<input type="hidden" id="remind01" name="remind01" value="<?php echo $_POST['remind01']; ?>">
   		  <input type="submit" value="変更" name="back"  style="WIDTH:70px; HEIGHT:30px;">
   		  </form>
 

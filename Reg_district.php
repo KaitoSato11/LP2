7ec1518884df09520b04886d34b72d6ef52b5764
htkz1035 <32477067+htkz1035@users.noreply.github.com>
2019-01-11 17:28:57 +0900

    地区登録　完成

--- Reg_district.php
+++ Reg_district.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <!--　～～～～～～～～～～～～～～～～地区登録画面表示～～～～～～～～～～～～～～～～～ -->
 
+<!-- HEADER -->
 <head>
 <meta charset="UTF-8" />
 <title>GDSS ゴミ出し支援システム</title>
@@ -15,31 +15,96 @@
       <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
 </div>
 
+<!-- メインコンテンツエリア -->
+  <div>
+    <!-- ここに各ページの中身いれてください -->
+    <div class="Main_Block1">
+      <a class="Block5" href="Mypage.php">
+        <h3>マイページに戻る</h3>
+      </a>
+    </div>
+
 <?php
 session_start();
+require_once('config.php');
+$id=$_SESSION['ID'];
 $check=0;
 $errorMessage="";
+$dist=null;
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $dist = $_POST['dist'];
-}
+  if(isset($_POST['dist1'])==true){
+    $dist = 1;
+    $dist_name=$_POST['dist1'];
+  }else if(isset($_POST['dist2'])==true){
+    $dist = 2;
+    $dist_name=$_POST['dist2'];
+  }else if(isset($_POST['dist3'])==true){
+    $dist = 3;
+    $dist_name=$_POST['dist3'];
+  }else if(isset($_POST['dist4'])==true){
+    $dist = 4;
+    $dist_name=$_POST['dist4'];
+  }else if(isset($_POST['dist5'])==true){
+    $dist = 5;
+    $dist_name=$_POST['dist5'];
+  }else if(isset($_POST['dist6'])){
+    $dist = 6;
+    $dist_name=$_POST['dist6'];
+    }
+    if(isset($_POST['dist'])==true){
+      $dist = $_POST['dist'];
+    }
+  }
 
-(isset($_POST['signup'])){
-$db_name = "mysql:db_name=gdss_db;host=localhost";
-$db_username = "root";
-$db_password = "";
-$db = new PDO($db_name, $db_username, $db_password);
-$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  try{
+  //データベース接続
+  $user = DB_USER;
+  $password = DB_PASSWORD;
+  $dbName = DB_NAME;
+  $host = DB_HOST;
+  $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+  $db = new PDO($dsn, $user, $password);
+  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stmt = $db -> perepare("INSERT INTO users(area_id) VALUES (:area_id)");
-$stmt -> bindValue(":area_id", $dist, PDO::PARAM_INT);
-$stmt -> execute();
-header("Location: Reg_district_comp.php");
-exit();
+  $sq = "SELECT *FROM gdss_db.users WHERE user_id = \"" .$id. "\" ";
+  $stm = $db->prepare($sq);
+  $stm->execute();
+  $res= $stm->fetchALL(PDO::FETCH_ASSOC);
+  $area = null;
+
+//データベースからarea_idを取得
+  foreach($res as $row){
+    if($row['area_id']==1){
+    $area="東1区";
+  }else if($row['area_id']==2){
+  $area="東2区";
+}else if($row['area_id']==3){
+$area="東3区";
+} else if($row['area_id']==4){
+$area="西1区";
+}else if($row['area_id']==5){
+$area="西2区";
+}else if($row['area_id']==6){
+$area="西3区";
 }
+  }
+
+//登録・変更ボタンが押されたときデータベースの値を更新
+  if(isset($_POST['signup'])){
+  $sql = "UPDATE gdss_db.users SET area_id =\"" .$dist. "\" WHERE user_id= \"" .$id. "\"";
+  $stmt = $db->prepare($sql);
+  $stmt -> execute();
+  header("Location:Reg_district_comp.php");
+  exit();
+    }
+  }catch(PDOException $Exception){
+    die('接続に失敗しました:' .$Exception->getMessage());
+  }
 
 //画面遷移
-if(isset($_POST["dist"])){
+if(isset($_POST["dist1"]) || isset($_POST["dist2"]) || isset($_POST["dist3"]) || isset($_POST["dist4"]) || isset($_POST["dist5"]) || isset($_POST["dist6"])){
 $check = 1;
 }
 if(isset($_POST["back"])){
@@ -47,68 +112,65 @@ if(isset($_POST["back"])){
 }
 ?>
 
-//地区登録画面
 <?php if($check=="0"){ ?>
+  <!-- 登録画面 -->
   <body>
-      <div class="content">
-        <img src="distmap.jpg">
-        <form action="" method="POST">
-        <table width="1024" height="350" align="center" cellspacing="0"  >
-  	<tr height="50" bgcolor="#8fc27a">
+   <div class="content">
+    <img src="distmap.jpg">
+    <form action="" method="POST">
+    <table>
+  	<tr>
   	  <th colspan="3">地区登録・変更</th>
   	</tr>
-  	<tr height="150" bgcolor="#deeed8">
-  	  <td align="center" valign="middle" width="270">
-  	    <p><input type="submit" id="dist" name="dist" style="background-color:#ebf1eb;width:100px;height:50px" value="東1区"></p>
-  	    </td>
-  	  <td align="center" valign="middle" width="270">
-  	    <p><input type="submit" id="dist" name="dist" style="background-color:#ebf1eb;width:100px;height:50px" value="東2区"></p>
-  	    </td>
-  	  <td align="center" valign="middle">
-  	    <p><input type="submit" id="dist" style="background-color:#ebf1eb;width:100px;height:50px" value="東3区"></p>
-  	    </td>
-  	  </tr>
-
-  	    <tr bgcolor="#deeed8">
-  	      <td align="center" valign="middle">
-  		<p><input type="submit" id="dist" name="dist" style="background-color:#ebf1eb;width:100px;height:50px" value="西1区"></p>
-  		</td>
-  	      <td align="center" valign="middle">
-  		<p><input type="submit" id="dist" name="dist" style="background-color:#ebf1eb;width:100px;height:50px" value="西2区"></p>
-  		</td>
-  	      <td align="center" valign="middle">
-  		<p><input type="submit" id="dist" name="dist" style="background-color:#ebf1eb;width:100px;height:50px" value="西3区"></p>
-  		</td>
-  	      </tr>
-  	      </table>
-        </form>
-      </div>
+  	<tr>
+  	  <td><p><input type="submit" id="dist1" name="dist1" value="東1区"></p></td>
+  	  <td><p><input type="submit" id="dist2" name="dist2" value="東2区"></p></td>
+  	  <td><p><input type="submit" id="dist3" name="dist3" value="東3区"></p></td>
+  	</tr>
+          
+  	<tr>
+  	  <td><p><input type="submit" id="dist4" name="dist4" value="西1区"></p></td>
+  	  <td><p><input type="submit" id="dist5" name="dist5" value="西2区"></p></td>
+  	  <td><p><input type="submit" id="dist6" name="dist6" value="西3区"></p></td>
+    </tr>
+  	 </table>
+     </form>
+   </div>
 </body>
 <?php } ?>
 
- //地区登録確認画面
 <?php if($check=="1"){ ?>
   <body>
-  <div class="content" align="center">
-  <h3><font color="red">この内容でよろしいですか？</font></h3>
-  <table align="center" border="1" cellspacing="0" width="600" height=100>
-  <tr bgcolor="#8fc27a" height="50"><th colspan="2">登録前地区</br></h3></th></tr>
-   <tr height="80"><td align="center">なし</td></tr>
+  <div class="content">
+  <table>
+    <tr><th><h3>地区登録・変更</h3></th></tr>
+    <tr><td>
+  <h3>地区を新たに変更・登録しますか。</h3>
+  <table>
+  <tr><th colspan="2">登録前地区</th></tr>
+   <tr><td><?php if($area!=null)echo $area;else print "なし"; ?></td></tr>
   </table>
-  <table align="center" border="1" cellspacing="0" width="600" height=100>
-<tr bgcolor="#8fc27a" height="50"><th colspan="2">変更後地区</br></h3></th></tr>
- <tr height="80"><td align="center"><?php echo $dist; ?></td></tr>
+  <table>
+<tr><th colspan="2">変更後地区</th></tr>
+ <tr><td><?php echo $dist_name; ?></td></tr>
 </table>
    <form action="" method="POST">
-     <input type="submit" value="変更・登録" name="signup" id="signup" style="WIDTH:70px; HEIGHT:30px;">
+     <input type="hidden" id="dist" name="dist" value="<?php echo $dist; ?>">
+     <input type="submit" value="変更・登録" name="signup" id="signup">
    </form>
 
    <form action="" method="POST">
-   <input type="submit" value="変更" name="back" id="back" name="back"  style="WIDTH:70px; HEIGHT:30px;">
+   <input type="submit" value="変更" name="back" id="back" name="back">
   </form>
-
+</td></tr>
+</table>
   </div>
   </body>
 <?php } ?>
-
+    
+<div class="footer">
+ <p class="title">GDSS</p>
+ <p class="company">L&P</p>
+</div>
+    
 </html>

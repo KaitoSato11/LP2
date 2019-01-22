c7caa1b6f1aeef3062330379bbaea02550215302
Doond <mdoond69h@gmail.com>
2019-01-22 15:30:56 +0900

    CSS用に改変

--- Reg_district_comp.php
+++ Reg_district_comp.php
@@ -1,36 +1,34 @@
 <!DOCTYPE HTML>
 <html  lang="ja">
-<head>
-<meta charset="UTF-8" />
-<link rel="icon" href="iconG.ico">
-<meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
-<link rel="stylesheet" href="design.css">
-</head>
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" href="iconG.ico">
+    <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+    <link rel="stylesheet" href="design.css">
+  </head>
 
-<div class="header">
+  <body>
+    <div class="header">
       <p class="title">GDSS</p>
       <p class="wayaku">ゴミ出し支援システム</p>
       <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
-</div>
-
-<body>
-  <div class="content">
-  <form action="Mypage.php" method="POST">
-   <table>
-   <tr>
-   <th>地区登録・変更</th>
-   </tr>
-   <td>
-   <p>登録が完了しました。</p>
-   <input type="submit" value="マイページに戻る">
-   </td>
-   </table>
- </form>
- </div>
-</body>
-
-<div class="footer">
- <p class="title">GDSS</p>
- <p class="company">L&P</p>
-</div>
+    </div>
+    <div class="content" id="contRgc">
+      <form action="Mypage.php" method="POST">
+        <table>
+          <tr>
+            <th><h2>地区登録・変更</h2></th>
+          </tr>
+          <td>
+            <p>登録が完了しました。</p>
+            <input type="submit" value="マイページに戻る">
+          </td>
+        </table>
+      </form>
+    </div>
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div>
+  </body>
 </html>

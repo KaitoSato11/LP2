f02430fd902610add6cfdd23ade2d63b4c91dfc2
Doond <mdoond69h@gmail.com>
2019-01-22 14:16:07 +0900

    CSS用に改変

--- Main.php
+++ Main.php
@@ -1,21 +1,39 @@
-<?php
+<!-- <?php
 echo "Hello Everyone.";
-?>
+?> -->
 
 <!doctype html>
 <html>
 	<head>
-		<title>Event Management System</title>
-		<meta charset="utf-8"/>
+		<meta charset="utf-8">
+    <title>GDSS ゴミ出し支援システム</title>
+    <link rel="icon" href="iconG.ico">
+    <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+    <link rel="stylesheet" href="design.css">
 	</head>
 	<body>
-		<div align="center">
-			<h1>Welcome to GDSS!</h1>
-			<a href="./Login.php">Login</a>
+		<!-- HEADER -->
+    <div class="header">
+      <p class="title">GDSS</p>
+      <p class="wayaku">ゴミ出し支援システム</p>
+      <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
+    </div>
+		<!-- メインコンテンツエリア -->
+    <div class="content">
+			<br><br><br>
+			<div align="center">
+				<h1>Welcome to GDSS!</h1>
+				<a href="./Login.php" id="logbut">ログイン</a>
+			</div>
+			<br><br><br>
+			<div align="right">
+				LastUpdate is 2018/12/7 16:30
+			</div>
 		</div>
-		<br><br><br>
-		<div align="right">
-			LastUpdate is 2018/12/7 16:30
-		</div>
+		<!-- FOOTER -->
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div>
 	</body>
 </html>

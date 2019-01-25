b7c306b66eb87c72699af5c2536c4e43e9c0de11
Doond <mdoond69h@gmail.com>
2019-01-25 17:09:32 +0900

    CSS用改変

--- Delete_user.php
+++ Delete_user.php
@@ -29,7 +29,7 @@ if(isset($_POST['del_user'])){
 		$stmt->execute(array($_SESSION['ID']));
 		header('Location: ./Delete_user_comp.php');
 		exit();
-		
+
         } catch(PDOException $e){
         $errorMessage = 'データベースエラー';
 	exit();
@@ -40,21 +40,40 @@ if(isset($_POST['del_user'])){
 
 <!doctype html>
 <html>
-    <head>
-    <title>利用者削除</title>
-		<meta charset="utf-8"/>
-    </head>
-    <body>
-        <!--規定画面-->
-        <p id="back_main"><input type="button" onClick="location.href='./Mypage.php'" value="マイページへ戻る" style="WIDTH:150px; HEIGHT:30px"></p>
-        <center><h1>利用者削除</h1></center>
-        <div align="center">
-            <h2><?php echo $errorMessage; ?></h2><br>
-	    <p><font size="4" color="#ff0000">このアカウントを削除します。<br> 削除したあと、このアカウントを復旧することはできません。</font></p>
-                <form action="" method="POST">
-                   <input type="submit" value="削除" id="del_user" name="del_user" style="WIDTH:150px; HEIGHT:30px">
-                </form>
-                <br>
-	</div>
-    </body>    
+  <head>
+    <title>GDSS ゴミ出し支援システム</title>
+    <link rel="icon" href="iconG.ico">
+    <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+    <link rel="stylesheet" href="design.css">
+  </head>
+  <body>
+    <!-- HEADER -->
+    <div class="header">
+      <p class="title">GDSS</p>
+      <p class="wayaku">ゴミ出し支援システム</p>
+      <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
+    </div>
+    <!-- メインコンテンツエリア -->
+    <p id="back_main"><input type="button" onClick="location.href='./Mypage.php'" value="マイページへ戻る" style="WIDTH:150px; HEIGHT:30px"></p>
+    <div class="content">
+      <!--規定画面-->
+      <div class="title">
+        <h2>ユーザ削除</h2>
+      </div>
+      <div align="center">
+        <h2><?php echo $errorMessage; ?></h2><br>
+        <p><font size="4" color="#ff0000">このアカウントを削除します。<br> 削除したあと、このアカウントを復旧することはできません。</font></p>
+        <form action="" method="POST">
+          <input type="submit" value="削除" id="del_user" name="del_user" style="WIDTH:150px; HEIGHT:30px">
+        </form>
+        <br>
+      </div>
+    </div>
+
+    <!-- FOOTER -->
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div>
+  </body>
 </html>

e9ce89d935c9a02822b810945ed7aaa1810700b6
Doond <mdoond69h@gmail.com>
2019-01-25 16:57:36 +0900

    CSS用改変

--- Mail.php
+++ Mail.php
@@ -1,28 +1,20 @@
 <!DOCTYPE HTML>
-
 <html lang="ja">
-<head>
-<meta charset="UTF-8" />
-<title>GDSS ゴミ出し支援システム</title>
-<link rel="icon" href="iconG.ico">
-<meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
-<link rel="stylesheet" href="design.css">
-</head>
+  <head>
+    <meta charset="UTF-8" />
+    <title>GDSS ゴミ出し支援システム</title>
+    <link rel="icon" href="iconG.ico">
+    <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+    <link rel="stylesheet" href="design.css">
+  </head>
 
-<div class="header">
+  <body>
+    <div class="header">
       <p class="title">GDSS</p>
       <p class="wayaku">ゴミ出し支援システム</p>
       <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
-</div>
-
-<!-- メインコンテンツエリア -->
-  <div>
-    <!-- ここに各ページの中身いれてください -->
-    <div class="Main_Block1">
-      <a class="Block5" href="Mypage.php">
-        <h3>マイページに戻る</h3>
-      </a>
     </div>
+
 
 <?php
 session_start();
@@ -81,59 +73,70 @@ if(isset($_POST['result'])==true){
 ?>
 
 <?php if($check == "0"){ ?>
-<body>
-  <div class="content">
-    <table>
-      <tr><th>メールリマインド設定</th></tr>
-      <td>
-        <h2><?php echo $errorMessage; ?></h2><br>
-	<p>メールリマインドを受け取りますか?</p>
-	<table>
-	  <tr><td>メールアドレス</td></tr>
-	  <td>
-      <form method="POST" action="" >
-      <input type="email" size="30" id="mail_adress" name="mail_address" id="mail_address" maxlength="50" placeholder="メールアドレス入力">
-      	      </td>
-      	    </table>
-      	    <input type="radio" id="remind01" name="remind01"  value="1">受け取る
-      	      <input type="radio" id="remind01" name="remind01"  value="0">受け取らない
-      	      </p>
-      	      <p><input type="submit" id="result" name="result"></p>
-      	      </form>
-      	    </td>
-      	  </table>
-      	</div>
-          </body>
+    <!-- メインコンテンツエリア -->
+    <div id="MlMain_Block1">
+      <a class="Mybutton" href="Mypage.php">
+        マイページに戻る
+      </a>
+    </div>
+    <div class="content" id="contMl">
+      <table class="MailT">
+        <tr class="top">
+          <th><h2>メールリマインド設定</h2></th>
+        </tr>
+        <td>
+          <h3><?php echo $errorMessage; ?></h3><br>
+          <p>メールリマインドを受け取りますか?</p>
+          <table class="MailT">
+            <tr><td>メールアドレス</td></tr>
+            <td>
+              <form method="POST" action="" >
+                <input type="email" size="30" id="mail_adress" name="mail_address" id="mail_address" maxlength="50" placeholder="メールアドレス入力">
+              </td>
+            </table>
+            <input type="radio" id="remind01" name="remind01"  value="1">受け取る
+            <input type="radio" id="remind01" name="remind01"  value="0">受け取らない
+            <p><input type="submit" id="result" name="result"></p>
+          </form>
+        </td>
+      </table>
+    </div>
 <?php } ?>
 
 <?php if($check == "1"){ ?>
-  <body>
-      <div class="content">
-        <br><h2><?php echo $errorMessage; ?></br></h2>
-  	<table>
-  	  <tr><td>メールアドレス</td><td><?php echo $_POST['mail_address']; ?></td></tr>
-  	  <tr><td>リマインド受け取り可否</td><td><?php if($_POST['remind01']== "1"){ ?>受け取る<?php }else{?> 受け取らない<?php } ?></td></tr>
+    <div id="MlMain_Block1">
+      <a class="Mybutton" href="Mypage.php">
+        マイページに戻る
+      </a>
+    </div>
+    <div class="content" id="contMl">
+      <div class="title">
+        <h2>メールリマインド設定</h2>
+      </div>
+      <center>
+        <h3><?php echo $errorMessage; ?></br></h3>
+        <table>
+          <tr><td>メールアドレス</td><td><?php echo $_POST['mail_address']; ?></td></tr>
+          <tr><td>リマインド受け取り可否</td><td><?php if($_POST['remind01']== "1"){ ?>受け取る<?php }else{?> 受け取らない<?php } ?></td></tr>
         </table>
-	      
         <form  action="" method="POST">
-  	 <input type="hidden" id="mail_address" name="mail_address" size="30" value="<?php echo $_POST['mail_address'];?>">
-  	 <input type="hidden" id="remind01" name="remind01" value="<?php echo $remind01; ?>">
-         <input type="submit" value="登録" name="signup"  style="WIDTH:70px; HEIGHT:30px;">
-  	</form>
-	      
-  	<form action="" method="POST">
-  	 <input type="hidden" id="mail_address" name="mail_address" size="30" value="<?php echo $_POST['mail_address']; ?>">
-         <input type="hidden" id="remind01" name="remind01" value="<?php echo $_POST['remind01']; ?>">
-         <input type="submit" value="変更" name="back">
+          <input type="hidden" id="mail_address" name="mail_address" size="30" value="<?php echo $_POST['mail_address'];?>">
+          <input type="hidden" id="remind01" name="remind01" value="<?php echo $remind01; ?>">
+          <input type="submit" value="登録" name="signup"  style="WIDTH:70px; HEIGHT:30px;">
         </form>
-
-	<br><h2><b>この内容でよろしいですか？</br></h2>	
-        </div>  		  
-  </body>
+        <form action="" method="POST">
+          <input type="hidden" id="mail_address" name="mail_address" size="30" value="<?php echo $_POST['mail_address']; ?>">
+          <input type="hidden" id="remind01" name="remind01" value="<?php echo $_POST['remind01']; ?>">
+          <input type="submit" value="変更" name="back">
+        </form>
+        <br><h2><b>この内容でよろしいですか？</br></h2>
+      </center>
+    </div>
 <?php } ?>
 
-<div class="footer">
- <p class="title">GDSS</p>
- <p class="company">L&P</p>
-</div>	
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div>
+  </body>
 </html>

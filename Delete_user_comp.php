4ec4c5ffb3aead220e6a80d29e79216bd27c1b2d
Doond <mdoond69h@gmail.com>
2019-01-25 17:14:38 +0900

    CSS用改変

--- Delete_user_comp.php
+++ Delete_user_comp.php
@@ -8,16 +8,38 @@ $_SESSION = array();
 
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>ユーザ削除完了</title>
-</head>
-<body>
-<center>
-<h2>ユーザ削除が完了しました</h2>
-</center>
-<div id="event">
-<a href="./Main.php">メインメニューへ戻る</a>
-</div>
-</body>
+  <head>
+    <title>GDSS ゴミ出し支援システム</title>
+    <link rel="icon" href="iconG.ico">
+    <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+    <link rel="stylesheet" href="design.css">
+  </head>
+  <body>
+    <!-- HEADER -->
+    <div class="header">
+      <p class="title">GDSS</p>
+      <p class="wayaku">ゴミ出し支援システム</p>
+      <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
+    </div>
+
+    <!-- メインコンテンツエリア -->
+    <div class="content">
+      <div class="title">
+        <h2>ユーザ削除</h2>
+      </div>
+      <center>
+        <br>
+        <h2>ユーザ削除が完了しました</h2>
+        <div id="event">
+          <a href="./Main.php">メインメニューへ戻る</a>
+        </div>
+      </center>
+    </div>
+
+    <!-- FOOTER -->
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div>
+  </body>
 </html>

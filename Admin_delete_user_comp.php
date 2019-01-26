c04765286dc83aed2114b42985f8a84bc20c064e
Doond <mdoond69h@gmail.com>
2019-01-26 16:33:15 +0900

    CSS用改変とコード手直し

--- Admin_delete_user_comp.php
+++ Admin_delete_user_comp.php
@@ -1,36 +1,34 @@
 <!DOCTYPE HTML>
 <html lang="ja">
-<head>
-<meta charset="UTF-8" />
-<title>GDSS ゴミ出し支援システム</title>
-<link rel="icon" href="iconG.ico">
-<meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
-<link rel="stylesheet" href="design.css">
-</head>
-
-<div class="header">
+  <head>
+    <meta charset="UTF-8" />
+    <title>GDSS ゴミ出し支援システム</title>
+    <link rel="icon" href="iconG.ico">
+    <meta name="description" content="高知県香美市土佐山田町を対象とした、ゴミ出しを支援するサイトです。">
+    <link rel="stylesheet" href="design.css">
+  </head>
+  <body>
+    <div class="header">
       <p class="title">GDSS</p>
       <p class="wayaku">ゴミ出し支援システム</p>
       <p class="desc">このサイトは、高知県香美市土佐山田町が対象となっています。</p>
-</div>
+    </div>
 
-<body>
-  <div class="content">
+    <div class="content" id="Acontduc">
+      <form action="Mypage.php" method="POST">
+        <table>
+          <tr><th class="duTtop">利用者削除</th></tr>
+          <tr><td>
+            <p style="margin:100px"><h3>削除が完了しました。</h3></p>
+            <input type="submit" value="マイページに戻る">
+          </td></tr>
+        </table>
+      </form>
+    </div>
 
-  <form action="Mypage.php" method="POST">
-   <table>
-   <tr><th>利用者削除</th></tr>
-   <tr><td>
-   <p style="margin:100px"><h3>削除が完了しました。</h3></p>
-   <input type="submit" value="マイページに戻る">
-   </td></tr>
-   </table>
- </form>
- </div>
-</body>
-
-<div class="footer">
- <p class="title">GDSS</p>
- <p class="company">L&P</p>
-</div>
+    <div class="footer">
+      <p class="title">GDSS</p>
+      <p class="company">L&P</p>
+    </div>
+  </body>
 </html>
